cb33a193131270fdfb5a270aaea51a0503ef84c7
h-ARTS <hi5hanan@yahoo.com>
2017-09-02 16:52:11 +0200

    Responsiveness and Links added

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Example = props => {
     return(
-        <h2>Example Component!!</h2>
+        <div>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are a few example locations to try out:</p>
+            <ol>
+                <li>
+                    <Link to="/?location=Zurich">Zurich, ZH</Link>
+                </li>
+                <li>
+                    <Link to="/?location=Locarno">Locarno, TI</Link>
+                </li>
+            </ol>
+        </div>
     );
 };
 
