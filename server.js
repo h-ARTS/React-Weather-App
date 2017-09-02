979402afffe78b4f0fb0c5191ecf1323fd2a44aa
h-ARTS <hi5hanan@yahoo.com>
2017-09-02 14:49:47 +0200

    Update middleware for localhost

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
-var express = require('express');
+let express = require('express');
+let app = express();
 
-// Create our app
-var app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 
-app.use(function (req, res, next) {
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    next();
-  } else {
-    res.redirect('http://' + req.hostname + req.url);
-  }
+app.use((req, res, next) => {
+	if(req.headers['x-forwarded-proto'] === 'https') {
+		res.redirect('http://' + req.hostname + req.url);
+	} else {
+		next();
+	}
 });
 
 app.use(express.static('public'));
 
-app.listen(PORT, function () {
+app.listen(PORT, () => {
   console.log('Express server is up on port ' + PORT);
 });
