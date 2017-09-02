7690a62ea2b0b0561120405cbbb92075d78d2fd8
h-ARTS <hi5hanan@yahoo.com>
2017-09-02 19:49:22 +0200

    Switch to SCSS

--- app/app.jsx
+++ app/app.jsx
@@ -7,7 +7,7 @@ import About from 'About';
 import Examples from 'Examples';
 
 require('style!css!foundation-sites/dist/foundation.min.css')
-require('style!css!applicationStyles')
+require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
   <Router history={hashHistory}>
